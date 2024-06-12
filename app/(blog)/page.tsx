307bf0d1bbf4e798367128efc7107389353eec21
“Darius” <darwest18@gmail.com>
2024-06-12 10:06:14 -0700

    customizations

---
+++ app/(blog)/page.tsx
@@ -0,0 +1,15 @@
+// ./app/(blog)/page.tsx
+
+import { SanityDocument } from "next-sanity";
+
+import Posts from "@/components/Posts";
+import { sanityFetch } from "@/sanity/lib/fetch";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
+
+export default async function Page() {
+  const posts = await sanityFetch<SanityDocument[]>({
+    query: POSTS_QUERY,
+  });
+
+  return <Posts posts={posts} />;
+}
---
+++ sanity/lib/queries.ts
@@ -0,0 +1,7 @@
+// ./sanity/lib/queries.ts
+
+import { groq } from "next-sanity";
+
+export const POSTS_QUERY = groq`*[_type == "post" && defined(slug)] | order(_createdAt desc)`;
+
+export const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
