d61d01fdcc9beafded197cf66c9d026589dee61e
“Darius” <darwest18@gmail.com>
2024-07-02 21:22:02 -0700

    support dashboard more

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,11 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 
-import blockContent from "./schemaTypes/blockContent";
-import category from "./schemaTypes/category";
-import post from "./schemaTypes/post";
-import author from "./schemaTypes/author";
 import engineer from "./schemaTypes/engineer";
+import blockContent from "./schemaTypes/blockContent";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent, engineer],
+  types: [blockContent, engineer],
 };
